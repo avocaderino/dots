fc67e0bd895f081a114d7489f59c88eab0118d5c
Sanjay P M <sanjaypmallar2108@gmail.com>
2021-10-25 17:11:57 +0530

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "Welcome to sanjay's files repository"
+
+echo "What is your username?"
+read name
+dest_dir="/home/$name/"
+
+echo "Would you like to download the files? [Y/n]"
+read files
+
+if [[ ${files,,} =~ y ]]
+    then
+        echo "Downloading the files"
+        cp -v Pictures/* $dest_dir/Pictures/
+        cp -v  dotfiles/zshrc $dest_dir/.zshrc
+        cp -v dotfiles/bash_aliases $dest_dir/.bash_aliases
+        cp -fv dotfiles/config/* $dest_dir/.config/
+        echo "Finished downloading the files"
+
+else
+    echo "Abort"
+
+fi
+
+exit
+
