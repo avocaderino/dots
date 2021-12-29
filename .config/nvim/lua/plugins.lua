6367fa562ffdd12a1df32a0c2bffa87061500b89
sanjapm <sanjaypmallar2108@gmail.com>
2021-12-29 23:34:25 +0530

    I'm mad

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,35 +2,38 @@
 
 return require("packer").startup(function()
 
-    -- plugin manager
-    use "wbthomason/packer.nvim"
+  -- plugin manager
+  use "wbthomason/packer.nvim"
 
-    -- movement
-    use "ggandor/lightspeed.nvim"
+  -- movement
+  use "ggandor/lightspeed.nvim"
 
-    -- telescope.nvim
-    use {
-        "nvim-telescope/telescope.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "kyazdani42/nvim-web-devicons",
-            "nvim-telescope/telescope-file-browser.nvim",
-            "nvim-telescope/telescope-fzf-native.nvim"
-        },
-        module = "telescope", cmd = "Telescope",
-        config = [[require "modules.telescope"]]
-    }
-    -- telescope deps and extensions
-    use {
-        -- plenary.nvim
-        {"nvim-lua/plenary.nvim", module = "plenary"},
-        -- colorful icons
-        {"kyazdani42/nvim-web-devicons", module = "nvim-web-devicons"},
-        -- file browser
+  -- telescope.nvim
+  use {
+    {
+      "nvim-telescope/telescope.nvim",
+      requires = {
+        "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-file-browser.nvim",
-        -- fuzzy finder
-        {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
+        "nvim-telescope/telescope-fzf-native.nvim"
+      },
+      module = "telescope", cmd = "Telescope",
+      config = [[require "modules.telescope"]]
+    },
+    {
+      "nvim-lua/plenary.nvim",
+      module = "plenary"
+    },
+    {
+      "kyazdani42/nvim-web-devicons",
+      module = "nvim-web-devicons"
+    },
+      "nvim-telescope/telescope-file-browser.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      run = "make"
     }
+  }
 
 end)
 
